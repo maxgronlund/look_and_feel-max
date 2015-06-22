9fd5e071e6a8f5f7b7f600314a4e274ea83130a0
Synthmax <max@synthmax.dk>
2015-06-22 23:42:15 +0200

    -

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name              "look_and_feel-tlq"
-maintainer        "Ben Dixon"
-maintainer_email  "ben@hillsbede.co.uk"
+name              "look_and_feel-staging"
+maintainer        "Max Grønlund"
+maintainer_email  "max@digiramp.com"
 description       "Installs tools to make the server nicer to work on, quite opinionated"
 version           "0.0.5"
 
-recipe "look_and_feel-tlq", "Adds visual flag to production environment as well as htop, vim and zip, also allows for the installation of additional locales"
+recipe "look_and_feel-staging", "Adds visual flag to production environment as well as htop, vim and zip, also allows for the installation of additional locales"
 
 supports "ubuntu"
 
