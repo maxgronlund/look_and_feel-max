79005fad26ace33bc530b93f53adbfdb2c34836b
Synthmax <max@synthmax.dk>
2015-06-22 23:52:55 +0200

    -

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "look_and_feel-staging"
 maintainer        "Max Grønlund"
 maintainer_email  "max@digiramp.com"
 description       "Installs tools to make the server nicer to work on, quite opinionated"
-version           "0.0.5"
+version           "0.0.6"
 
 recipe "look_and_feel-staging", "Adds visual flag to production environment as well as htop, vim and zip, also allows for the installation of additional locales"
 
