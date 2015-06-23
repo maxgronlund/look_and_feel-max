c50ef325b3cd5d25df061b9992f890cc8adc5b2d
Synthmax <max@synthmax.dk>
2015-06-23 09:48:53 +0200

    -

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,7 +13,7 @@ locales "Add locales" do
 end
 
 # Add a banner to ssh login if we're in the production environment
-if node[:environment] == 'production'
+if node[:environment] == 'staging'
   sshd_config = '/etc/ssh/sshd_config'
 
   seds = []
@@ -29,6 +29,40 @@ if node[:environment] == 'production'
     group 'root'
     mode '0644'
     source 'staging_ssh_banner.erb'
+  end
+
+  bash 'Adding visual flags for production environment' do
+    user 'root'
+    code <<-EOC
+      #{seds.map { |rx| "sed -i '#{rx}' #{sshd_config}" }.join("\n")}
+      #{echos.map { |e| %Q{echo "#{e}" >> #{sshd_config}} }.join("\n")}
+    EOC
+  end
+
+  service 'ssh' do
+    provider Chef::Provider::Service::Upstart
+    supports :restart => true
+    action :restart
+  end
+end
+
+# Add a banner to ssh login if we're in the production environment
+if node[:environment] == 'production'
+  sshd_config = '/etc/ssh/sshd_config'
+
+  seds = []
+  echos = ["\n"]
+
+  banner_path = '/etc/ssh_banner'
+
+  seds << 's/^Banner/#Banner/g'
+  echos << "Banner #{banner_path}"
+
+  template banner_path do
+    owner 'root'
+    group 'root'
+    mode '0644'
+    source 'production_ssh_banner.erb'
   end
 
   bash 'Adding visual flags for production environment' do
